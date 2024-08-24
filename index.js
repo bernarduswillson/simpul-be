93cae2059c39c64e44bb31fc893de7095f5f6e39
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-08-24 23:57:30 +0700

    remove: env

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.delete("/messages/:messageId", (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
