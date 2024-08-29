aea1400160c987fdb2501e918c547327cd27e75d
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-08-29 16:16:32 +0700

    feat: edit tasks

--- index.js
+++ index.js
@@ -183,8 +183,8 @@ const initialMessages = [
 
 const initialTasks = [
   {
-    id: 1,
-    userId: 1,
+    id: "1",
+    userId: "1",
     name: "Close off Case #012920- RODRIGUES, Amiguel",
     date: "2024-09-24T05:19:00Z",
     description: "Closing off this case since this application has been cancelled. No one really understand how this case could possibly be cancelled. The options and the documents within this document were totally a guaranteed for a success!",
@@ -192,8 +192,8 @@ const initialTasks = [
     isDone: false,
   },
   {
-    id: 2,
-    userId: 1,
+    id: "2",
+    userId: "1",
     name: "Set up documentation report for several Cases : Case 145443, Case 192829 and Case 182203",
     date: "2024-09-25T05:19:00Z",
     description: "All Cases must include all payment transactions, all documents and forms filled. All conversations in comments and messages in channels and emails should be provided as well in.",
@@ -201,8 +201,8 @@ const initialTasks = [
     isDone: false,
   },
   {
-    id: 3,
-    userId: 1,
+    id: "3",
+    userId: "1",
     name: "Set up appointment with Dr Blake",
     date: "2024-09-26T05:19:00Z",
     description: "",
@@ -210,8 +210,8 @@ const initialTasks = [
     isDone: false,
   },
   {
-    id: 4,
-    userId: 1,
+    id: "4",
+    userId: "1",
     name: "Contact Mr Caleb - video conference?",
     date: "2024-08-24T05:19:00Z",
     description: "This is a description",
@@ -219,8 +219,8 @@ const initialTasks = [
     isDone: true,
   },
   {
-    id: 5,
-    userId: 1,
+    id: "5",
+    userId: "1",
     name: "Assign 3 homework to Client A",
     date: "2024-08-10T05:19:00Z",
     description: "This is a description",
@@ -477,6 +477,18 @@ app.get("/api/tasks/:userId", (req, res) => {
   const userId = req.params.userId;
   const userTasks = tasks.filter((task) => task.userId == userId);
   res.json(formatResponse("success", "Tasks retrieved successfully", userTasks));
+});
+
+// UPDATE date or description of a task by task ID
+app.put("/api/tasks/:taskId", (req, res) => {
+  const task = tasks.find((task) => task.id == req.params.taskId);
+  if (task) {
+    task.date = req.body.date || task.date;
+    task.description = req.body.description || task.description;
+    res.json(formatResponse("success", "Task updated successfully", task));
+  } else {
+    res.status(404).json(formatResponse("error", "Task not found", null));
+  }
 });
 
 
